ecd49427c423ccdc361d8f669f53af8766e33b52
hatakeyama <hatakeyama.chie@d-publishing.jp>
2026-07-03 14:48:40 +0900

    Initial Commit

---
+++ WetherNow/WetherNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  WetherNowApp.swift
+//  WetherNow
+//
+//  Created by hatakeyama.chie on 2026/07/03.
+//
+
+import SwiftUI
+
+@main
+struct WetherNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
